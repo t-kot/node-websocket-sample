892fccdb5b9bca0d046c2cf59efc615e6ab9a471
kotohata-imac <t.kotohata@gmail.com>
2012-08-19 19:15:31 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , socketIO = require('socket.io')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+// Routes
+app.get('/', routes.index);
+
+var server = http.createServer(app).listen(app.get('port'),function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+//Wait the connection of client
+var io = socketIO.listen(server);
+
+//When the clients connect
+io.sockets.on('connection', function(socket){
+  console.log('connection');
+
+  //Receive messaeg
+  socket.on('message', function(msg){
+
+    //Submit all connecting clients
+    console.log('message');
+    io.sockets.emit('message', msg);
+  });
+
+  //When the clients disconnect
+  socket.on('disconnect', function(){
+    console.log('disconnect');
+  });
+});
